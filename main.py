f503500579d00b0c00d9506b61c105b71a3f4a59
Oleksii Panpukha <58779018+3xecut0r@users.noreply.github.com>
2023-06-08 15:33:57 +0200

    summary

--- main.py
+++ main.py
@@ -11,53 +11,41 @@ def parser(string: str) -> list:
 
 def find_match(param):
     quotes = Quotes.objects()
-    result = []
+    result = ["List of quotes: "]
     if isinstance(param, list):
         for el in param:
             for quote in quotes:
-                author = Authors.objects(fullname=quote.author)
-                matches = re.match(el, str(quote.tags)) or re.match(el, str(author.fullname)) or re.match(el, str(quote.quote))
+                tags = quote.tags
+                matches = re.match(el, str(quote.author.fullname), re.IGNORECASE) \
+                          or re.match(el, str(quote.quote), re.IGNORECASE)
+                if any(re.match(el, tag, re.IGNORECASE) for tag in tags):
+                    result.append(quote.quote)
                 if matches:
                     result.append(matches)
     else:
         for quote in quotes:
-            author = Authors.objects(fullname=quote.author)
-            matches = re.match(param, str(quote.tags)) or re.match(param, str(author.fullname)) or re.match(param, str(quote.quote))
+            tags = quote.tags
+            matches = re.match(param, str(quote.author.fullname), re.IGNORECASE) \
+                      or re.match(param, str(quote.quote), re.IGNORECASE)
+            if any(re.match(param, tag, re.IGNORECASE) for tag in tags):
+                result.append(quote.quote)
             if matches:
-                result.append(matches)
+                result.append(quote.quote)
     return result
 
+
 if __name__ == '__main__':
-    quotes = Quotes.objects()
-    for quote in quotes:
-        print(str(quote.author.fullname))
-    # while True:
-    #     arg = input('-->').strip().split(':')
-    #
-    #     if arg[0] == 'name':
-    #         author = arg[1].strip()
-    #         quotes = Quotes.objects(author__fullname=author).all()
-    #         for quote in quotes:
-    #             print(quote.quote.encode('utf-8'))
-    #     elif arg[0] == 'tag':
-    #         tag = arg[1].strip()
-    #         quotes = Quotes.objects(tags=tag).all()
-    #         for quote in quotes:
-    #             print(quote.quote.encode('utf-8'))
-    #     elif arg[0] == 'tags':
-    #         tags = arg[1].strip().split(',')
-    #         quotes = Quotes.objects(tags__in=tags).all()
-    #         for quote in quotes:
-    #             print(quote.quote.encode('utf-8'))
-    #     elif arg[0] == 'exit':
-    #         break
 
-
-
-        # command = arg[0]
-        # arg = parser(arg[1].strip())
-        # match = find_match(arg)
-        # for m in match:
-        #     print(m.group().decode('utf-8'))
-        #
+    while True:
+        arg = input('-->').strip().split(':')
+        command = arg[0]
+        try:
+            arg = parser(arg[1].strip())
+            match = find_match(arg)
+            for m in match:
+                print(m)
+        except IndexError:
+            pass
+        if command == 'exit':
+            break
 
--- seeds.py
+++ seeds.py
@@ -1,45 +1,20 @@
 from models import Authors, Quotes
-from pymongo import MongoClient
-from connect import mongo_user, mongodb_pass, domain
 import json
 
 
-client = MongoClient(f'mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/?retryWrites=true&w=majority')
-db = client['test']
-
 with open('authors.json') as f:
     authors_data = json.load(f)
-    db.authors.insert_many(authors_data)
 
 with open('quotes.json') as f:
     quotes_data = json.load(f)
-    db.quotes.insert_many(quotes_data)
 
 
-# for author_data in authors_data:
-#     author = Authors(**author_data)
-#     author.save()
-#
-# for quote_data in quotes_data:
-#     quote = Quotes(**quote_data)
-#     quote.save()
+for author_data in authors_data:
+    author = Authors(**author_data)
+    author.save()
 
-
-
-
-
-
-
-
-
-
-# tag = Tag(name='Purchases')
-#
-# record1 = Record(description='Buying sausage')
-# record2 = Record(description='Buying milk')
-# record3 = Record(description='Buying oil')
-#
-# Notes(name='Shopping', records=[record1, record2, record3], tags=[tag, ]).save()
-#
-# Notes(name='Going to the movies', records=[Record(description='Went to see the Avengers'), ], tags=[Tag(name='Fun'), ]).save()
-#
+for quote_data in quotes_data:
+    author_name = quote_data.get('author')
+    author = Authors.objects.get(fullname=author_name) if author_name else None
+    quote = Quotes(author=author, quote=quote_data['quote'], tags=quote_data.get('tags'))
+    quote.save()
